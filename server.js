a19d69934b19b64d90820b249a4a204a6fcc1bde
chocoluffy <luffy.yu@mail.utoronto.ca>
2016-07-27 22:26:25 -0400

    Add slack integration

--- server.js
+++ server.js
@@ -2,24 +2,65 @@ var request = require('request');
 var express = require('express');
 
 var app = express();
-var port = 3016;
-var feedURL = "https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=-1&q=https://news.ycombinator.com/rss"
+var port = 3010;
+var feedURL = "https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=-1&q=http://feeds.bbci.co.uk/news/rss.xml"
 
 app.get('/', function(req, res){
-	request({
-	    url: feedURL, //URL to hit
-	    method: 'GET',
-	}, function(error, response, body){
-		if(error) {
-	        console.log(error);
-	    } 
-	    else {
-	        if(response.statusCode == 200){
-				data = JSON.parse(body).responseData.feed.entries
-				console.log(data);
+	var mostRecentFeeds = []
+
+	var updateFeeds = function(){
+		request({
+		    url: feedURL, //URL to hit
+		    method: 'GET',
+		}, function(error, response, body){
+			if(error) {
+		        console.log(error);
+		    } 
+		    else {
+		        if(response.statusCode == 200){
+					data = JSON.parse(body).responseData.feed.entries;
+					if(mostRecentFeeds.length > 0){
+						// if mostRecentFeeds contains previous feeds.
+						var mostRecentTitle = mostRecentFeeds[0]['title'];
+						var stopIndex = 0;
+						for(var i=0; i<data.length; i++){
+							if(data[i]['title'].localeCompare(mostRecentTitle) == 0){
+								// no update yet.
+								break;
+							}
+							else{
+								stopIndex = i; 
+							}
+						}
+						// update mostRecentFeeds with data sliced till stopIndex.
+						mostRecentFeeds = data.slice(0, stopIndex);
+
+						if(mostRecentFeeds.length > 0){
+							// forwards latest feeds to slack bot.
+							for(var i=0; i<mostRecentFeeds.length; i++){
+								request.post({
+								  url: 'https://hooks.slack.com/services/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',
+								  headers: {'content-type' : 'application/json'},
+								  data: {'text' : mostRecentFeeds[i]['title'] + ' ' + mostRecentFeeds[i]['link']}
+								}, function(error, response, body){
+								  console.log(body);
+								});
+							}
+						}
+					}
+					else{
+						// init mostRecentFeeds array.
+						mostRecentFeeds = data;
+					}
+					console.log(mostRecentFeeds);
+				}
 			}
-		}
-	})
+		})
+	}
+	
+	setInterval(updateFeeds, 500000);
+
+	res.send("No worries, it's running.");
 });
 
 app.listen(port);
