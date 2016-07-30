ea8fcf89b79da10ad8631a0f986c578fe6f33b4a
chocoluffy <luffy.yu@mail.utoronto.ca>
2016-07-29 20:17:41 -0400

    Add channel name

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var Slack = require('node-slack');
 
-var slack = new Slack('https://hooks.slack.com/services/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');
+var slack = new Slack('https://hooks.slack.com/services/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');
 
 var app = express();
-var port = 3010;
+var port = 3002;
 var feedURL = "https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=-1&q=http://feeds.bbci.co.uk/news/rss.xml"
 
 app.get('/', function(req, res){
+	console.log("hi");
 	slack.send({
 		text: "hi"
 	})
